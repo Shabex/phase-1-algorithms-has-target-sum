80f1ee523723d06415980455f80980ef48b530fa
Shabex <shabsibrah@gmail.com>
2022-06-17 18:18:52 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i =0; i<array.length-1; i++){
+    for (let j = i+1; j<array.length; j++){
+      if (array[i]+array[j] === target){
+        return true
+      }
+    }
+
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
+//Time Complexity: 0(n^2)
 
 /* 
   Add your pseudocode here
@@ -13,6 +22,16 @@ function hasTargetSum(array, target) {
 /*
   Add written explanation of your solution here
 */
+//The function has two arguements, an array and a target
+//Two elements from the array when summed up should give us the target
+//sort the array in non-decreasing order 
+// initialize two index variable to find the candidate element in the array
+// i for index 0 and j for the second last index i.e j = array.length-1
+//use for loop to pass in the candidates of the array
+
+
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
